6837154c70a2817d0f5e58d90257f0cc24b7ed69
Alexander Fillbrunn <alxndr_f@web.de>
2013-04-03 14:23:32 +0200

    Proper keys for config
    o Server was nor working because config was read with wrong keys

--- redisml/server/server.py
+++ redisml/server/server.py
@@ -3,8 +3,8 @@ from redis import Redis
 
 class Server:
     def __init__(self, config):
-        self.redis = Redis(config['host'], port=config['port'], db=config['db'])
-        self.matrix_factory = matrix.MatrixFactory(self.redis, config['block_size'])
+        self.redis = Redis(config['redis']['host'], port=config['redis']['port'], db=config['redis']['db'])
+        self.matrix_factory = matrix.MatrixFactory(self.redis, config['matrix']['block_size'])
         
     def matrix_from_numpy(self, mat, name=''):
         return self.matrix_factory.matrix_from_numpy(mat, name)
