c6dda666b5e545a7d33cda7e574542020431a6c2
Alexander Fillbrunn <alxndr_f@web.de>
2013-04-05 21:06:04 +0200

    Command line args for worker

--- worker.py
+++ worker.py
@@ -1,41 +1,21 @@
 from redisml.worker.management.base import Worker
 from redisml.worker import worker_settings
 import redis
+import sys
 
-"""
-import redis
-from client import command_handler
-import json
+host = 'localhost'
+port = 6379
+db = 0
 
-r = redis.Redis(host='localhost', port=6379, db=0)
-pubsub = r.pubsub()
-pubsub.subscribe('c_jobs')
-stop = False
-
-while not stop:
-    job_id = r.spop('free_jobs')
-
-    if job_id != None:
-        job = json.loads(r.get('job:' + str(job_id)))
-        
-        try:
-            command_handler.execute(r, job['cmd'])
-        except Exception, e:
-            print 'Error: ' + str(e)
-            r.publish('c_results', json.dumps({'id' : job_id, 'success' : False, 'reason' : 'fail'}))
-            
-        r.sadd('finished_jobs', job_id)
-        r.publish('c_results', json.dumps({'id' : job_id, 'success' : True}))
-    else:
-        #Block until a new job gets published
-        print 'No jobs, listening...'
-        for j in pubsub.listen():
-            data = j['data'].split(' ')
-            if data[0] == 'JOB':
-                if not r.sismember('finished_jobs', data[1]):
-                    break
-"""
+if len(sys.argv) > 2:
+    for i in range(1,len(sys.argv),2):
+        if sys.argv[i] == 'h' and i <= len(sys.argv)-2:
+            host = sys.argv[i+1]
+        if sys.argv[i] == 'p' and i <= len(sys.argv)-2:
+            port = int(sys.argv[i+1])
+        if sys.argv[i] == 'db' and i <= len(sys.argv)-2:
+            db = int(sys.argv[i+1])
 
 worker_settings.initLogging()
-c = Worker('localhost', 6379, 0)
+c = Worker(host, port, db)
 c.run()
