23dff41676d3b3f2f24242a82a670a34a4750d1c
Alexander Fillbrunn <alxndr_f@web.de>
2013-04-14 20:30:22 +0200

    Updated the pagerank sample
    o multiply() was renamed to dot()
    o Matrices with ones can be created without numpy

--- samples/pagerank.py
+++ samples/pagerank.py
@@ -10,18 +10,16 @@ d = 0.8
 v_quadratic_error = 0.001
 
 v_ = np.random.rand(5,1)
-last_v_ = np.ones((5,1)) * 999
 M_ = np.matrix([ [0,     0,      0,      0,   1],
                  [0,     0,      1,      0,   0],
                  [0,     0,      0,      0,   0],
                  [0,     1,      0,      0,   0],
                  [1,     0,      0,      1,   0]])
-ones_ = np.ones((N, N))
 
 v = s.matrix_from_numpy(v_)
-last_v = s.matrix_from_numpy(last_v_)
+last_v = s.matrix_from_scalar(999, 5, 1)
 M = s.matrix_from_numpy(M_)
-ones = s.matrix_from_numpy(ones_)
+ones = s.matrix_from_scalar(1, N, N)
 
 v = v / v.vector_norm(2)
 
@@ -30,7 +28,7 @@ M_hat = (M * d) + ones * ((1 - d) / N)
 count = 0
 while (v - last_v).vector_norm(2) > v_quadratic_error:
     last_v = v
-    v = M_hat.multiply(v)
+    v = M_hat.dot(v)
     v = v / v.vector_norm(2)
     count += 1
 
